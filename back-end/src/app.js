a01a9ec6dd9aa03e00a80491c12a04eb83700ee0
RafaelVSs <felzinpb@gmail.com>
2024-03-16 11:37:54 -0300

    Criação das rotas

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -5,6 +5,7 @@ import logger from "morgan";
 import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
 
+
 const app = express();
 
 app.use(logger("dev"));
@@ -14,5 +15,13 @@ app.use(cookieParser());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+/************************************* 
+ROTAS
+**************************************/
+
+import clienteRouter from './routes/cliente.js'
+app.use('/clientes', clienteRouter)
+
 
 export default app;
--- back-end/src/controller/cliente.js
+++ back-end/src/controller/cliente.js
@@ -1,5 +1,21 @@
-import prisma from "../database/client";
+import prisma from "../database/client.js";
 
 const controller = {}; // Objeto vazio
+
+controller.create = async function(req, res) { //Só pode usar awai em uma função async
+    try {
+        await prisma.cliente.create({ data: req.body }) //await vai executar a linha de baixo quando obter uma resposta da entidade externa, nesse caso, nosso banco
+
+        // Envia uma resposta de sucesso ao front-end
+        // HTTP 201: Created
+        res.status(201).end()
+    }
+    catch(error) {
+        console.error(error)
+        //HTTP 500: Internal Server Error
+        res.status(500).end()
+    }
+
+}
 
 export default controller
