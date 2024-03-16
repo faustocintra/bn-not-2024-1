d04c77e4b6842719fbcce3b00eb3a989b92cce47
izabelatrc <izabela.coimbra@fatec.sp.gov.br>
2024-03-16 11:32:19 -0300

    (16/03) Primeiro POST

--- back-end/src/controllers/cliente.js
+++ back-end/src/controllers/cliente.js
@@ -1,5 +1,20 @@
-import prisma from '../database/client.js';
+import prisma from "../database/client.js";
 
-const controller = {}; //objeto vazio
+const controller = {}  // Objeto vazio
 
-export default controller
+controller.create = async function(req, res) {
+    try {
+        await prisma.cliente.create({ data: req.body })
+
+        // Envia uma resposta de sucesso ao front-end
+        // HTTP 201: Created
+        res.status(201).end()
+    }
+    catch(error) {
+        console.error(error)
+        // HTTP 500: Internal Server Error
+        res.status(500).end()
+    }
+}
+
+export default controller
