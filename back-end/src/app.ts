498912085fa599cf1bcacd58c43d948c57e3c99d
kefflen <kefflenjava@hotmail.com>
2024-03-20 20:11:59 -0300

    feat: create express error middleware

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,13 +1,14 @@
 import cookieParser from "cookie-parser"
-import express, { json, urlencoded } from "express"
+import express, { NextFunction, Request, Response, json, urlencoded } from "express"
 import logger from "morgan"
 
+import 'express-async-errors'
 import mongoose from "mongoose"
 import { env } from "./env"
 import indexRouter from "./routes"
 import clientRouter from "./routes/client"
 import usersRouter from "./routes/users"
-
+import { ZodError } from "zod"
 const app = express();
 
 mongoose.connect(env.DATABASE_URL).then(() => {
@@ -25,6 +26,13 @@ app.use(cookieParser());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use('/clientes', clientRouter)
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof ZodError) {
+    return res.status(400).json({ message: error.errors })
+  }
+  return res.status(500).json({ message: error.message })
+})
 
 export default app;
 
