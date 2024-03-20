f1feaa825e474c6f4d795268876b170535150832
Claudio Matos <claudiomatos@gmail.com>
2024-03-20 20:32:54 -0300

    (20/03) Troca da biblioteca Prisma pela Mongoose

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,11 +1,18 @@
 import express, { json, urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import dotenv from 'dotenv';
+//Carrega as variáveis do arquivo .env
+//no objeto global process.env
+dotenv.config();
 
 import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
 
 const app = express();
+
+import mongoose from 'mongoose'
+mongoose.connect(process.env.DATABASE_URL);
 
 app.use(logger("dev"));
 app.use(json());
