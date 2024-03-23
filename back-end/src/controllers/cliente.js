7f9e3151f58229855203c0a7ed379908e88e9ed2
IgoROweN <igorowen2015@hotmail.com>
2024-03-23 11:02:31 -0300

    FaltaProgamarOBanco

--- back-end/src/controllers/cliente.js
+++ back-end/src/controllers/cliente.js
@@ -17,4 +17,62 @@ controller.create = async function(req, res) {
   }
 }
 
+controller.retriveAll = async function (req, res) {
+  try {
+    const result = await Cliente.find().sort({ neme: 'asc' })
+    // HTTP 200: OK (implicito)
+    res.send(result)
+  }
+  catch(error) {
+    console.error(error)
+    // HTTP 500: Internal Server Error
+    res.status(500).end()
+  }
+}
+
+controller.retriveOne = async function(req, res) {
+  try{
+    const result = await Cliente.findById(req.params.id)
+    // Documento não encontrado ~> HTTP 220: OK (implicito)
+    if (result) res.send(result)
+    // Documento encontrado ~> HTTP 404: Not Found
+    else res.status(404).end()
+  }
+  catch(error) {
+    console.error(error)
+    // HTTP 500: Internal Server Error
+    res.status(500).end()
+  }
+}
+
+controller.update = async function(req, res) {
+  try{
+    const result = await Cliente.findByIdAndUpdate(req.params.id, req.body)
+    // Documento encontrado e atualizado ~> HTTP 204: No Content
+    if(result) res.status(204).end()
+    // Documento não encontratado (e não atualizado) ~> HTTP 404: Not Found
+    else res.status(404).end()
+  }
+  catch(error) {
+    console.error(error)
+    // HTTP 500: Internal Server Error
+    res.status(500).end()
+  }
+}
+
+controller.delete = async function(req, res) {
+  try{
+    const result = await Cliente.findByIdAndDelete(req.params.id)
+    // Documento encontrado e excluido ~> HTTP 204: No Content
+    if(result) res.status(204).end()
+    // Documento não encontrado (e não excluido) ~> HTTP 404: Not Found
+    else res.status(404).end()
+  }
+  catch(error) {
+    console.error(error)
+    // HTTP 500: Internal Server Error
+    res.status(500).end()
+  }
+}
+
 export default controller
--- back-end/src/routes/cliente.js
+++ back-end/src/routes/cliente.js
@@ -4,5 +4,9 @@ import controller from '../controllers/cliente.js'
 const router = Router()
 
 router.post('/', controller.create)
+router.get('/', controller.retriveAll)
+router.get('/:id', controller.retriveOne)
+router.put('/:id', controller.update)
+router.delete('/:id', controller.delete)
 
 export default router
