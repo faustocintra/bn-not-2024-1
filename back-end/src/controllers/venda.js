d52b5e7fb85ed7cb9072bfc0a0f7f03b6b964e79
BrunoJose-dev <brunojose0112@gmail.com>
2024-04-03 20:28:28 -0300

    (03/04) Início das rotinas de CRUD dos itens da venda

--- back-end/src/controllers/venda.js
+++ back-end/src/controllers/venda.js
@@ -1,4 +1,3 @@
-import { populate } from 'dotenv'
 import Venda from '../models/Venda.js'
 
 const controller = {}   // Objeto vazio
@@ -18,49 +17,93 @@ controller.create = async function(req, res) {
   }
 }
 
-controller.retrievAll = async function(req, res){
+controller.retrieveAll = async function(req, res) {
   try {
-    const query = Venda.find().sort({ descricao: 'asc'})
-    //verifica se o parãmetro 'pop_fornecedor' foi passado na url
-    //e, em caso positivo, acrescenta o populate() á consulta
-    if('pop_fornecedor' in req.query)query.populate('fornecedor')
+    const query = Venda.find().sort({ num: 'asc' })
+
+    // Verifica se o parâmetro 'pop_cliente' foi passado na URL
+    // e, em caso positivo, acrescenta o populate() à consulta
+    if('pop_cliente' in req.query) query.populate('cliente')
+
+    // Verifica se o parâmetro 'pop_produto' foi passado na URL
+    // e, em caso positivo, acrescenta o populate() à consulta
+    if('pop_produto' in req.query) query.populate('itens.produto')
+
     const result = await query.exec()
-    //http 200: ok (implícito)
+
+    // HTTP 200: OK (implícito)
     res.send(result)
   }
-  catch(error){
+  catch(error) {
     console.error(error)
-    //http 500: Internal Server Error
+    // HTTP 500: Internal Server Error
     res.status(500).end()
   }
 }
 
-controller.retrieveOne = async function(req, res){
-  try{
-    const query = Venda.find().sort({ descricao: 'asc'})
-    //verifica se o parãmetro 'pop_fornecedor' foi passado na url
-    //e, em caso positivo, acrescenta o populate() á consulta
-    if('pop_fornecedor' in req.query)query.populate('fornecedor')
+controller.retrieveOne = async function(req, res) {
+  try {
+    const query = Venda.findById(req.params.id)
+
+    // Verifica se o parâmetro 'pop_cliente' foi passado na URL
+    // e, em caso positivo, acrescenta o populate() à consulta
+    if('pop_cliente' in req.query) query.populate('cliente')
+
+    // Verifica se o parâmetro 'pop_produto' foi passado na URL
+    // e, em caso positivo, acrescenta o populate() à consulta
+    if('pop_produto' in req.query) query.populate('itens.produto')
+
     const result = await query.exec()
 
+    // Documento encontrado ~> HTTP 200: OK (implícito)
     if(result) res.send(result)
-    //Documento não encontrado ~> HTTP 404: Not Found
-    else res.status(404).end()
+    // Documento não encontrado ~> HTTP 404: Not Found
+    else res.status(404).end()  
   }
-  catch(error){
+  catch(error) {
     console.error(error)
-    //HTTP 500: INternal Server Error
+    // HTTP 500: Internal Server Error
     res.status(500).end()
   }
 }
 
-controller.update = async function(req, res){
-  try{
+controller.update = async function(req, res) {
+  try {
     const result = await Venda.findByIdAndUpdate(req.params.id, req.body)
-    //Documento encontrado e atualizado ~> HTTP 204: No Content
+    // Documento encontrado e atualizado ~> HTTP 204: No Content
     if(result) res.status(204).end()
-    //Documento não encontrado (e não atualizado) ~> HTTP 404: Not Found
+    // Documento não encontrado (e não atualizado) ~> HTTP 404: Not Found
     else res.status(404).end()
+  }
+  catch(error) {
+    console.error(error)
+    // HTTP 500: Internal Server Error
+    res.status(500).end()
+  }
+}
+
+controller.delete = async function(req, res) {
+  try {
+    const result = await Venda.findByIdAndDelete(req.params.id)
+    // Documento encontrado e excluído ~> HTTP 204: No Content
+    if(result) resstatus(204).end()
+    // Documento não encontrado (e não excluído) ~> HTTP 404: Not Found
+    else res.status(404).end()
+  }
+  catch(error) {
+    console.error(error)
+    // HTTP 500: Internal Server Error
+    res.status(500).end()
+  }
+}
+controller.retrieveALLItems = async function(req, res) {
+  try{
+    //1) Procurando pela venda na qual moo novo item está inserido
+    const venda = await Venda.findById(req.params.id)
+     //2)Se a vendfa for enconmtrada, retorna http 404:  not found
+    if (!venda) return res.status(404).end()
+    // Senão, retorna o vetor venda.itens com http 200:ok (implícito)
+   else res.send(venda.items)
   }
   catch(error){
     console.error(error)
@@ -68,19 +111,16 @@ controller.update = async function(req, res){
     res.status(500).end()
   }
 }
-
-controller.delete = async function(req, res){
+controller.retrieveOneItem = async function(req, res) {
   try{
-    const result = await Venda.findByIdAndDelete(req.params.id)
-    //Documento encontrado e excluído ~> HTTP 204: No Content
-    if(result) res.status(204).end()
-    //Documento não encontrado (e não excluído) ~> HTTP 404: Not Found
-    else res.status(404).end()
+    //1) Procurando pela venda na qual moo novo item está inserido
+    const venda = await Venda.findById(req.params.id)
+     //2)Se a vendfa for enconmtrada, retorna http 404:  not found
+    if (!venda) return res.status(404).end()
+    //3) Procura o item específico dentor do vetor venda.itens
+    const item = venda.itens.id(req.params.itemId)
+    //4)Se o item não for encontrado, retorna http 404:  not found
   }
-  catch(error){
-    console.error(error)
-    //HTTP 500: INternal Server Error
-    res.status(500).end()
-  }
+  
 }
 export default controller
--- back-end/src/routes/venda.js
+++ back-end/src/routes/venda.js
@@ -4,7 +4,7 @@ import controller from "../controllers/venda.js"
 const router = Router()
 
 router.post('/', controller.create)
-router.get('/', controller.retrievAll)
+router.get('/', controller.retrieveAll)
 router.get('/:id', controller.retrieveOne)
 router.put('/:id', controller.update)
 router.delete('/:id', controller.delete)
