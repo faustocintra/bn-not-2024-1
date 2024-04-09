86c432981a92820ca656727f099ab37b93e697c8
Paula Abib <paula.tws@hotmail.com>
2024-04-09 20:58:43 -0300

    Revert previous two commits

--- back-end/src/routes/cliente.js
+++ back-end/src/routes/cliente.js
@@ -4,9 +4,5 @@ import controller from '../controllers/cliente.js'
 const router = Router()
 
 router.post('/', controller.create)
-router.get('/', controller.retrieveAll)
-router.get('/:id', controller.retrieveOne)
-router.put('/:id', controller.update)
-router.delete('/:id', controller.delete)
 
 export default router
