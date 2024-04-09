c26b1ff3ae1a8e1b18689a1dce7f166bf83f717a
Leovpf <leovpf@hotmail.com>
2024-04-09 11:57:13 -0300

    Inicio projeto back end 13-03

--- README.md
+++ README.md
@@ -1,16 +1,4 @@
-# Criação do projeto back-end
-npx @aka-demy/create-express-app
+# bn-not-2024-1
 
-Respostas às perguntas:
-* Install package...? y
-* Give a name for the app: back-end
-* Choose a language: JavaScript
-* Choose a template engine: None
-* Choose a package manager: npm
-
-# Mudando para a pasta do projeto
-cd back-end
-
-# Instalando as dependências necessárias
-npm install mongoose
-npm install dotenv
+LEONARDO VICTOR PEREIRA FERREIRA RA: 1091392323052 
+Repositório da disciplina Banco de Dados - Não Relacional, 3º semestre DSM Fatec Franca 2024/1
--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,8 +1,3 @@
-import dotenv from 'dotenv'
-// Carrega as variáveis do arquivo .env
-// no objeto global process.env
-dotenv.config()
-
 import express, { json, urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
@@ -12,9 +7,6 @@ import usersRouter from "./routes/users.js";
 
 const app = express();
 
-import mongoose from 'mongoose'
-mongoose.connect(process.env.DATABASE_URL)
-
 app.use(logger("dev"));
 app.use(json());
 app.use(urlencoded({ extended: false }));
@@ -22,21 +14,5 @@ app.use(cookieParser());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
-/***************************************************
- * ROTAS
-***************************************************/
-
-import clienteRouter from './routes/cliente.js'
-app.use('/clientes', clienteRouter)
-
-import fornecedorRouter from './routes/fornecedor.js'
-app.use('/fornecedores', fornecedorRouter)
-
-import produtoRouter from './routes/produto.js'
-app.use('/produtos', produtoRouter)
-
-import vendaRouter from './routes/venda.js'
-app.use('/vendas', vendaRouter)
 
 export default app;
