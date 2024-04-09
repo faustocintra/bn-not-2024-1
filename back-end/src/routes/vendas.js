9c61f25cbd06db784872ff3b2441ddeb2c5c4597
Claudio Matos <claudiomatos@gmail.com>
2024-04-09 20:59:49 -0300

    (06/04 Término do projeto back-end de exemplo)

--- back-end/src/routes/vendas.js
+++ back-end/src/routes/vendas.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import controller from '../controllers/venda.js'
+import controller from '../controllers/vendas.js'
 
 const router = Router()
 
@@ -12,5 +12,7 @@ router.delete('/:id', controller.delete)
 router.post('/:id/itens', controller.createItem)
 router.get('/:id/itens', controller.retrieveAllItems)
 router.get('/:id/itens/:itemId', controller.retrieveOneItem)
+router.put('/:id/itens/:itemId', controller.updateItem)
+router.delete('/:id/itens/:itemId', controller.deleteItem)
 
 export default router
