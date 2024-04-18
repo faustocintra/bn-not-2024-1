21f3b0933258a9e8d98630a9b2184ad7eeb441fd
Bruno Algarte Inacio <br_inacio@magazineluiza.intranet>
2024-04-18 00:20:22 -0300

    Entrega Tarefa 1

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,3 +1,8 @@
+import dotenv from 'dotenv'
+// Carrega as variáveis do arquivo .env
+// no objeto global process.env
+dotenv.config()
+
 import express, { json, urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
@@ -7,6 +12,9 @@ import usersRouter from "./routes/users.js";
 
 const app = express();
 
+import mongoose from 'mongoose'
+mongoose.connect(process.env.DATABASE_URL)
+
 app.use(logger("dev"));
 app.use(json());
 app.use(urlencoded({ extended: false }));
@@ -15,11 +23,20 @@ app.use(cookieParser());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-/************************************* 
-ROTAS
-**************************************/
+/***************************************************
+ * ROTAS
+***************************************************/
 
 import clienteRouter from './routes/cliente.js'
 app.use('/clientes', clienteRouter)
 
-export default app;
+import fornecedorRouter from './routes/fornecedor.js'
+app.use('/fornecedores', fornecedorRouter)
+
+import produtoRouter from './routes/produto.js'
+app.use('/produtos', produtoRouter)
+
+import vendaRouter from './routes/venda.js'
+app.use('/vendas', vendaRouter)
+
+export default app;
--- back-end/src/routes/cliente.js
+++ back-end/src/routes/cliente.js
@@ -3,6 +3,10 @@ import controller from '../controllers/cliente.js'
 
 const router = Router()
 
-router.post('/cliente', controller.create)
+router.post('/', controller.create)
+router.get('/', controller.retrieveAll)
+router.get('/:id', controller.retrieveOne)
+router.put('/:id', controller.update)
+router.delete('/:id', controller.delete)
 
-export default router
+export default router
